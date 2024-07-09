8f4efee9509ee2275a6dc7f718958564d9355084
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-07-09 11:14:17 -0300

    atualização dos tamplates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qpm202othao"
+    "revision": "0.2924dehgfsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/templates/small/SmallHeader.vue
+++ src/components/templates/small/SmallHeader.vue
@@ -1,3 +1,24 @@
+<script setup>
+import LogoTitle from '@/components/templates/LogoTitle.vue';
+</script>
+
 <template>
-    <h2>Small Header</h2>
-  </template>
+  <logo-title class="pl-0" />
+  <div class="icons">
+    <i class="mdi mdi-magnify" />
+    <i class="mdi mdi-account-circle-outline" />
+    <i class="mdi mdi-cart-outline" />
+  </div>
+</template>
+
+<style scoped>
+.pl-0 {
+  padding-left: 0rem;
+}
+
+.icons {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+}
+</style>
